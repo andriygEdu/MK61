1ca61ea411c2fbf3c13341a75daa3309f356ab46
Andriy Gushuley <andriy.gushuley@gmail.com>
2012-02-05 15:45:37 +0200

    Initial Commit

---
+++ MK61/MK61AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MK61AppDelegate.h
+//  MK61
+//
+//  Created by Andriy Gushuley on 05.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MK61AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MK61/MK61ViewController.h
@@ -0,0 +1,13 @@
+//
+//  MK61ViewController.h
+//  MK61
+//
+//  Created by Andriy Gushuley on 05.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MK61ViewController : UIViewController
+
+@end
